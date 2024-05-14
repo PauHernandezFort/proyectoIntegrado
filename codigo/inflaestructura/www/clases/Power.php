f12aeeccfb0a18e04dc32bdcd057387e4f09d02a
Daniel Alos Prats <daalpr@floridauniversitaria.es>
2024-05-14 16:58:22 +0200

    solucionado

--- codigo/inflaestructura/www/clases/Power.php
+++ codigo/inflaestructura/www/clases/Power.php
@@ -1,42 +1,45 @@
 <?php
 
-class Power extends Connection{
+class Power extends Connection {
     private $nombrePoder;
     private $daño;
     private $coste;
 
-    public function __construct($nombrePoder){
-        $conn= $this->getConn();
-        $this->nombrePoder=$nombrePoder;
-        $query = 'SELECT * FROM Poder WHERE nombrePoder = "Volar"';
-        $result = mysqli_query($this->conn, $query);
-        var_dump($result);
+    public function __construct($nombrePoder) {
+        parent::__construct();
+        $conn = $this->getConn();
+        $this->nombrePoder = $nombrePoder;
+        $query = "SELECT * FROM Poder WHERE nombrePoder = '$nombrePoder'";
+        $result = mysqli_query($conn, $query);
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        if ($row) {
+            $this->nombrePoder = $row['nombrePoder'];
+            $this->daño = $row['daño'];
+            $this->coste = $row['coste'];
+        } else {
+            echo "No rows found for $nombrePoder";
+        }
+    }
 
+    public function __toString() {
+        return "Nombre del poder: " . $this->nombrePoder . ", Daño: " . $this->daño . ", Coste: " . $this->coste;
 
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    $this->nombrePoder = $row['nombrePoder'];
-    $this->daño = $row['daño'];
-    $this->coste = $row['coste'];
+    }
 
-}
-
-    public function getNombre(){
+    public function getNombre() {
         return $this->nombrePoder;
     }
-    public function setDaño($nombrePoder){
-        $this->nombrePoder=$nombrePoder;
+
+    public function setDaño($daño) {
+        $this->daño = $daño;
     }
-    public function getDaño(){
+
+    public function getDaño() {
         return $this->daño;
     }
-   
-    public function getCoste(){
+
+    public function getCoste() {
         return $this->coste;
     }
-    
-    public function getmodelId($conn) {
-        
-    }
-
-
 }
+?>
--- codigo/inflaestructura/www/indexayuda.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "autoloader.php";
-$modelo = new Power(' Volar');
-
-
-
-?>
