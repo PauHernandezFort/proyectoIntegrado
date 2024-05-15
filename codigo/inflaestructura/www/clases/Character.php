3874839419017a7365b893d38f95181c87d02759
sintaxyserror <joagsa@floridauniversitaria.es>
2024-05-15 12:30:46 +0200

    modificacion personaje

--- codigo/inflaestructura/www/clases/Character.php
+++ codigo/inflaestructura/www/clases/Character.php
@@ -1,29 +1,33 @@
 <?php
 class Character extends Connection{
 
+    protected $nombre; 
     protected $energia; 
-    protected $vida; 
-    protected $nonbre; 
-    protected $daño; 
-    protected $nombrePoder1;
-    protected $nombrePoder2; 
-    protected $nombrePoder3; 
+    protected $correocuenta; 
+    protected $vida;
+    protected $daño;  
+
 
     public function __construct($nombre){
         parent::__construct();
         $this->nombre = $nombre;
-        $query = "SELECT * FROM Personaje1 WHERE nombre = '$nombre'"; 
+        $query = "SELECT * FROM Personaje WHERE nombre = '$nombre'"; 
         $result =  mysqli_query($this->conn, $query);
         $row = $result->fetch_array(MYSQLI_ASSOC);
         if ($row) {
-            $this->energia = $row['energia'];
-            $this->vida = $row['vida'];
             $this->nombre = $row['nombre'];
-            $this->daño = $row['daño'];
+            $this->energia = $row['energia'];
+            $this->correocuenta = $row['correocuenta'];
+            $this->vida = $row['vida'];
+            $this->daño = $row['daño']; 
         } else {
             echo "No rows found for $nombre";
         }
         
+    }
+
+    public function getnombre(){
+        return $this->nombre; 
     }
 
 
@@ -31,60 +35,46 @@ class Character extends Connection{
         return $this->energia; 
     }
 
+    public function getCorreoCuenta(){
+        return $this->correocuenta; 
+    }
+
     public function getVida(){
         return $this->vida;
     }
 
-    public function getnombre(){
-        return $this->nombre; 
-    }
+   
 
     public function getDaño(){
         return $this->daño; 
     }
 
-    public function getNombrePoder1(){
-        return $this->nombrePoder1;
-    }
-
-    public function getNombrePoder2(){
-        return $this->nombrePoder2;
-    }
-
-    public function getNombrePoder3(){
-        return $this->nombrePoder3; 
-    }
 
     public function importar(){
         $personajes = fopen("personajes.csv", "r") or die("Unable to open file!");
         while (!feof($personajes)) {
             $data = fgetcsv($personajes);
-            $query1 = "INSERT INTO Personaje1 (energia, vida, nombre, daño, NombrePoder)
-             VALUES ('$data[1]', '$data[2]', '$data[3]', '$data[4]', '$data[5]');";
-            $result1 = mysqli_query($this->conn, $query1);
-            $query2 = "INSERT INTO Personaje2 (energia, vida, nombre, daño, NombrePoder)
-            VALUES ('$data[1]', '$data[2]', '$data[3]', '$data[4]', '$data[5]');";
-           $result2 = mysqli_query($this->conn, $query2);
+            $query = "INSERT INTO Personaje (nombre, energia, correocuenta, vida, daño)
+             VALUES ('$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]');";
+            $result = mysqli_query($this->conn, $query);
+            
         }
         fclose($personajes);
     }
 
     public function borrar(){
-        $query1 = "DELETE FROM Personaje1;";
-        $result1 = mysqli_query($this->conn, $query1);
-        $query2 = "DELETE FROM Personaje2;";
-        $result2 = mysqli_query($this->conn, $query2);
+        $query = "DELETE FROM Personaje;";
+        $result = mysqli_query($this->conn, $query);
     }
 
     public function init(){
         $this->borrar();
-        $query1 = "SELECT COUNT(*) FROM Personaje1;";
-        $result1 = mysqli_query($this->conn, $query1)->fetch_row()[0];
-        $query2 = "SELECT COUNT(*) FROM Personaje2;";
-        $result2 = mysqli_query($this->conn, $query2)->fetch_row()[0];
-        if ($result1 == 0 && $result2 == 0){
+        $query = "SELECT COUNT(*) FROM Personaje;";
+        $result = mysqli_query($this->conn, $query)->fetch_row()[0];
+        if ($result == 0 ){
             $this->importar();
         }
+        echo "la tabla personaje ha sido poblada correctamente";
     }
 
 
--- codigo/inflaestructura/www/home.php
+++ codigo/inflaestructura/www/home.php
@@ -1,4 +1,4 @@
 <?php
-
+require_once "autoloader.php"; 
 echo "hola";
 ?>
