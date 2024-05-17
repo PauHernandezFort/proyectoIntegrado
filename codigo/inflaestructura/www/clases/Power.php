e026afba2a31d29699719aec4deafb71676869ec
IvanContador <ivcoso@floridauniversitaria.es>
2024-05-17 11:56:09 +0200

    hola

--- codigo/inflaestructura/www/clases/Power.php
+++ codigo/inflaestructura/www/clases/Power.php
@@ -20,7 +20,7 @@ class Power extends Connection {
             $this->coste = $row['coste'];
             $this->descripcion = $row["descripcion"];
         } else {
-          //  cho "No rows found for $nombrePoder";
+          //  echo "No rows found for $nombrePoder";
         }
     }
 
