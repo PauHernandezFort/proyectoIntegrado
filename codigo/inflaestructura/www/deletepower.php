edcb405af025f3ab9321c52a2cc8db3583ac14d8
Dani Alos <alospratsdani@gmail.com>
2024-05-17 13:28:10 +0200

    deletepower

--- codigo/inflaestructura/www/deletepower.php
+++ codigo/inflaestructura/www/deletepower.php
@@ -1,5 +1,5 @@
 <?php
-require_once "autoloader.php";
+/*require_once "autoloader.php";
 
 $modelo = new Connection();
 $conn = $modelo->getConn();
@@ -13,7 +13,10 @@ if(isset($_GET['nombrePoder']) && !empty($_GET['nombrePoder'])) {
         header("Location: home.php");
         exit; 
     }
-}
+}*/
+require_once "autoloader.php";
+$power= new Power('alah');
+$array =$power->getAllPowers();
 
 ?>
 <!DOCTYPE html>
@@ -23,21 +26,31 @@ if(isset($_GET['nombrePoder']) && !empty($_GET['nombrePoder'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eliminar Poder</title>
 </head>
+
+
 <body>
+   
+    <form action="procesarEliminar.php" method="POST">
     <h1>Eliminar Poder</h1>
-    <label for="nombrePoder">Seleccione el nombre del poder a eliminar:</label>
-    <select id="nombrePoder" name="nombrePoder">
-        <option value="Poder1"></option>
- 
-    </select>
-    <br><br>
-    <button onclick="eliminarPoder()">Eliminar</button>
+        <label for="poder1">Poder:</label>
+        <select id="poder1" name="poder1">
+        </select><br><br>
+        <input type="submit" value="Crear Personaje">
+    </form>
 
     <script>
-        function eliminarPoder() {
-            var nombrePoder = document.getElementById("nombrePoder").value;
+          let array = <?php echo json_encode($array); ?>;
+          window.onload = agregarPoderes;
+        function agregarPoderes() {
+            for (var i = 0; i < array.length; i++) {
+                var option = document.createElement("option");
+                option.text = array[i];
+                option.value = array[i];
+                var selectPoder1 = document.getElementById("poder1");
+
+                slectPoder1.add(option.cloneNode(true));
+                }
         }
-        // no se seguir
     </script>
 </body>
 </html>
