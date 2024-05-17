b56f71695dacec8436ea6cab71b403e6384e0dc9
Pau Hernàndez <pahefo@floridauniversitaria.es>
2024-05-17 09:42:29 +0200

    arreglando fallos

--- codigo/inflaestructura/www/signUp.php
+++ codigo/inflaestructura/www/signUp.php
@@ -1,3 +1,10 @@
+<?php
+require_once "autoloader.php";
+$security = new Security();
+$security->singUp();
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -11,6 +18,7 @@
     <meta name="theme-color" content="#343a40">
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <style>
+      
         body {
             font-family: 'Press Start 2P', cursive;
             background-image: url('singUp.png');
@@ -22,47 +30,46 @@
             justify-content: center;
             align-items: center;
             height: 100vh;
+            font-family: 'Press Start 2P', cursive;
         }
-
         #form_container {
             width: 300px;
             padding: 20px;
             background-color: rgba(255, 255, 255, 0.8);
             border-radius: 10px;
         }
-
         .form_description {
             text-align: center;
         }
-
+        .element input {
+            width: 100%;
+        }
         .buttons {
             text-align: center;
         }
-
         .button_text {
             margin: 10px auto;
         }
     </style>
 </head>
-
 <body id="main_body">
     <div id="form_container" class="container">
         <h1 class="text-center">Sign Up</h1>
-        <form class="appnitro" method="post" action="login.php">
+        <form class="appnitro" method="post" action="">
             <div class="form-group">
                 <label for="userName">User Name</label>
-                <input name="userName" class="form-control" type="text" maxlength="255" value="">
+                <input name="userName" class="form-control" type="text" maxlength="255" value="" />
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input name="email" class="form-control" type="text" maxlength="255" value="">
+                <input name="email" class="form-control" type="text" maxlength="255" value="" />
             </div>
             <div class="form-group">
                 <label for="userPassword">User Password</label>
-                <input name="userPassword" class="form-control" type="password" maxlength="255" value="">
+                <input name="userPassword" class="form-control" type="password" maxlength="255" value="" />
             </div>
             <div class="buttons">
-                <input id="saveForm" class="btn btn-primary button_text" type="submit" name="submit" value="Sign Up">
+                <input id="saveForm" class="btn btn-primary" type="submit" name="submit" value="Log In" />
             </div>
         </form>
     </div>
