edd2009706de6d004367b5f2718b239c27669794
Carlos Guijarro Mínguez <cagumi@floridauniversitaria.es>
2024-05-21 12:03:00 +0200

    Home de Dani

--- codigo/inflaestructura/www/home.php
+++ codigo/inflaestructura/www/home.php
@@ -56,18 +56,15 @@ $lineas= mysqli_num_rows($result);
     </a>
      <a href="contrincante.php" class="fixed-button-right">
         <button class="btn btn-success">BUSCAR CONTRINCANTE</button>
-<<<<<<< HEAD
     </a>
     <a href="init.php" class="fixed-button-center">
     <button class="btn btn-secundary" style="background-image: url('cofre.gif'); background-size: contain; background-repeat: no-repeat; background-position: center; height: 80px; width: 80px; text-indent: -9999px;">PODERES</button>
     </a>
-=======
         <a href="init.php" class="fixed-button-center">
     <button class="btn btn-secundary" style="background-image: url('cofre.gif');
      background-size: contain; background-repeat: no-repeat; background-position: center; 
      height: 80px; width: 80px; text-indent: -9999px;">PODERES</button>
 </a>
->>>>>>> dc92fee948eb6ee2aa5525203c0b7d98b79109e8
     <script>
 document.addEventListener("DOMContentLoaded", function() {
     let correo = <?php echo json_encode($correo); ?>;
