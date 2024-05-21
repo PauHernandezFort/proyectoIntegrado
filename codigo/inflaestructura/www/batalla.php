6c2586fdb38fda059492956f0746cff9ed7e4c37
Pau Hernàndez <pahefo@floridauniversitaria.es>
2024-05-21 12:46:03 +0200

    mas avances en batalla

--- codigo/inflaestructura/www/batalla.php
+++ codigo/inflaestructura/www/batalla.php
@@ -1,12 +1,10 @@
 <?php
-
 $nombrePoder1j1 = "fumar";
-$nombrePoder2j1 = "saltar";
+$nombrePoder2j1 = "morder";
 $nombrePoder3j1 = "golpear";
 $nombrePoder1j2 = "morder";
-$nombrePoder2j2 = "patear";
-$nombrePoder3j2 = "empujar";
-
+$nombrePoder2j2 = "fumar";
+$nombrePoder3j2 = "aseaewqew";
 
 $vidaj1 = 100;
 $vidaj2 = 100;
@@ -22,59 +20,62 @@ $energiaj2 = 50;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Batalla</title>
     <style>
-       body {
-        margin: 0;
-        padding: 0;
-        display: flex;
-        flex-direction: column;
-       
-    }
-    #container {
-        flex: 1;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .barra {
-        height: 10px;
-        margin-bottom:30px;
-    }
-    #barraVerde {
-        background-color: green;
-        width: 50%;
-        padding: 10px; 
-        
-       
-    }
-
-    #barraBlanca {
-        flex: 1;
-        background-color: white;
-    }
-    #barraAzul {
-        background-color: blue;
-        width: 50%;
-        padding: 10px; 
-      
-    }
-    #botones {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 20px;
-    }
-    .boton {
-        padding: 10px 20px;
-        background-color: #ccc;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-    h3 {
-    color: red; 
-    text-align: left;
+         body {
+     margin: 0;
+     padding: 0;
      display: flex;
-    }
+     flex-direction: column;
+    
+ }
+ #container {
+     flex: 1;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+ }
+ .barra {
+     height: 10px;
+     margin-bottom:30px;
+ }
+ #barraVerde {
+     background-color: green;
+     width: 49%;
+     padding: 10px; 
+     
+    
+ }
+ #barraBlanca {
+     flex: 1;
+     background-color: white;
+ }
+ #barraAzul {
+     background-color: blue;
+     width: 49%;
+     padding: 10px; 
+   
+ }
+ #botones {
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     margin: 20px;
+ }
+ .boton {
+     padding: 10px 20px;
+     background-color: #ccc;
+     border: none;
+     border-radius: 5px;
+     cursor: pointer;
+ }
+ h3 {
+ color: red; 
+ text-align: left;
+  display: flex;
+     
+  }
+        #energia1, #energia2 {
+            margin: 10px;
+        }
     </style>
 </head>
 <body>
@@ -83,25 +84,32 @@ $energiaj2 = 50;
         <div id="barraBlanca" class="barra"></div>
         <div id="barraAzul" class="barra"></div>
     </div>
+    <div>
+        <h5>Energia Jugador 1: <span id="energia1"><?php echo $energiaj1; ?></span></h5>
+        <h5>Energia Jugador 2: <span id="energia2"><?php echo $energiaj2; ?></span></h5>
+    </div>
     <h3 id="error"></h3>
-    <h3 id="turno"></h3>
-    <h3 id="pepe"></h3> <br>
-    <h3 id="pepe2"></h3>
+    <h3 id="turno">Turno: <span id="numTurno">1</span></h3>
+    <h3 id="pepe">Vida Jugador 2: <?php echo $vidaj2; ?></h3>
+    <h3 id="pepe2">Vida Jugador 1: <?php echo $vidaj1; ?></h3>
 
     <div id="botones">
         <div>
             <button class="boton" onclick="botonClickeado('<?php echo $nombrePoder1j1; ?>', 1)"><?php echo $nombrePoder1j1; ?></button>
             <button class="boton" onclick="botonClickeado('<?php echo $nombrePoder2j1; ?>', 1)"><?php echo $nombrePoder2j1; ?></button>
             <button class="boton" onclick="botonClickeado('<?php echo $nombrePoder3j1; ?>', 1)"><?php echo $nombrePoder3j1; ?></button>
+            <button class="boton" onclick="botonClickeado('saltarTurno', 1)">Saltar turno</button>
         </div>
         <div>
             <button class="boton" onclick="botonClickeado('<?php echo $nombrePoder1j2; ?>', 2)"><?php echo $nombrePoder1j2; ?></button>
             <button class="boton" onclick="botonClickeado('<?php echo $nombrePoder2j2; ?>', 2)"><?php echo $nombrePoder2j2; ?></button>
             <button class="boton" onclick="botonClickeado('<?php echo $nombrePoder3j2; ?>', 2)"><?php echo $nombrePoder3j2; ?></button>
+            <button class="boton" onclick="botonClickeado( 'saltarTurno', 2)">Saltar turno</button>
         </div>
     </div>
 
     <script>
+        let array =[];
         let vidaj1 = <?php echo $vidaj1; ?>;
         let vidaj2 = <?php echo $vidaj2; ?>;
         let dañoj1 = <?php echo $dañoj1; ?>;
@@ -111,104 +119,185 @@ $energiaj2 = 50;
         let turno = 1;
 
         function botonClickeado(poder, jugador) {
+            if (jugador === 1 && turno % 2 === 1) {
+                realizarAccion(poder, 1);
+            } else if (jugador === 2 && turno % 2 === 0) {
+                realizarAccion(poder, 2);
+            } else {
+                document.getElementById("error").innerText = "No es tu turno";
+            }
+        }
+
+        function realizarAccion(poder, jugador) {
             if (poder === "fumar") {
-                fumar(turno, jugador);
+                fumar(jugador);
             } else if (poder === "saltar") {
                 // Lógica para el poder "saltar"
             } else if (poder === "golpear") {
                 // Lógica para el poder "golpear"
             } else if (poder === "morder") {
-                morder(turno, jugador);
+                morder(jugador);
             } else if (poder === "patear") {
                 // Lógica para el poder "patear"
             } else if (poder === "empujar") {
                 // Lógica para el poder "empujar"
+            } else if (pdoer= "saltarTurno"){
+                saltarTurno(jugador)
             }
+            document.getElementById("numTurno").innerText = turno;
         }
 
-        function cambiarTamaño(nuevo, jugador) {
-            turno += 1;
-            let barraVerde = document.getElementById("barraVerde");
-            let barraAzul = document.getElementById("barraAzul");
-            let nuevoPorcentaje = (nuevo / 2) + "%";
+        function actualizarBarraVida(jugador) {
+            let porcentaje;
             if (jugador === 1) {
-                barraAzul.style.width = nuevoPorcentaje;
-                document.getElementById("pepe").innerHTML = vidaj2;
+                porcentaje = ((vidaj1 * 100) / <?php echo $vidaj1; ?>)/2;
+                document.getElementById("barraVerde").style.width = porcentaje + "%";
+                document.getElementById("pepe2").innerText = "Vida Jugador 1: " + vidaj1;
             } else {
-                barraVerde.style.width = nuevoPorcentaje;
-                document.getElementById("pepe2").innerHTML = vidaj1;
+                porcentaje = ((vidaj2 * 100) / <?php echo $vidaj2; ?>)/2;
+                document.getElementById("barraAzul").style.width = porcentaje + "%";
+                document.getElementById("pepe").innerText = "Vida Jugador 2: " + vidaj2;
             }
+            comprobar()
         }
 
-        function fumar(turno, jugador) {
+        function fumar(jugador) {
             let dañoPoder = 5;
-            if (jugador === 1) {
-                if (turno % 2 === 0) {
-                    document.getElementById("error").innerHTML = "No es tu turno";
-                } else {
-                    let daño = dañoPoder + (dañoj1 + vidaj2) * 0.30;
-                    let dañoRedondeado = Math.ceil(daño);
-                    vidaj2 -= dañoRedondeado;
-                    let porcentaje = (vidaj2 * 100) / <?php echo $vidaj2; ?>;
-                    if (vidaj2 < 0) {
-                        porcentaje = 0;
-                        vidaj2 = 0;
-                    }
-                    document.getElementById("error").innerHTML = "";
-                    cambiarTamaño(porcentaje, 1);
-                }
-            } else {
-                if (turno % 2 !== 0) {
-                    document.getElementById("error").innerHTML = "No es tu turno";
-                } else {
-                    let daño = dañoPoder + (dañoj2 + vidaj1) * 0.30;
-                    let dañoRedondeado = Math.ceil(daño);
-                    vidaj1 -= dañoRedondeado;
-                    let porcentaje = (vidaj1 * 100) / <?php echo $vidaj1; ?>;
-                    if (vidaj1 < 0) {
-                        porcentaje = 0;
-                        vidaj1 = 0;
-                    }
-                    document.getElementById("error").innerHTML = "";
-                    cambiarTamaño(porcentaje, 2);
-                }
-            }
-        }
+            let energiaPoder = 10;
+            let errorElement = document.getElementById("error");
 
-        function morder(turno, jugador) {
-            let dañoPoder = 5;
             if (jugador === 1) {
-                if (turno % 2 === 0) {
-                    document.getElementById("error").innerHTML = "No es tu turno";
-                } else {
-                    let daño = dañoPoder + (dañoj1 + vidaj2) * 0.30;
-                    let dañoRedondeado = Math.ceil(daño);
-                    vidaj2 -= dañoRedondeado;
-                    let porcentaje = (vidaj2 * 100) / <?php echo $vidaj2; ?>;
-                    if (vidaj2 < 0) {
-                        porcentaje = 0;
-                        vidaj2 = 0;
-                    }
-                    document.getElementById("error").innerHTML = "";
-                    cambiarTamaño(porcentaje, 1);
+                if (energiaj1 < energiaPoder) {
+                    errorElement.innerText = "No tienes suficiente energía";
+                    return;
                 }
+                let daño = dañoPoder + (dañoj1 + vidaj2) * 0.30;
+                let dañoRedondeado = Math.ceil(daño);
+                vidaj2 -= dañoRedondeado;
+
+                if (vidaj2 < 0) {
+                    vidaj2 = 0;
+                }
+
+                energiaj1 -= energiaPoder;
+                energiaj1 += 5;
+                let arrayTurno= ['fumar',1,dañoRedondeado,energiaPoder];
+                array.push(arrayTurno);
+                document.getElementById("energia1").innerText = energiaj1;
+                actualizarBarraVida(2);
             } else {
-                if (turno % 2 !== 0) {
-                    document.getElementById("error").innerHTML = "No es tu turno";
-                } else {
-                    let daño = dañoPoder + (dañoj2 + vidaj1) * 0.30;
-                    let dañoRedondeado = Math.ceil(daño);
-                    vidaj1 -= dañoRedondeado;
-                    let porcentaje = (vidaj1 * 100) / <?php echo $vidaj1; ?>;
-                    if (vidaj1 < 0) {
-                        porcentaje = 0;
-                        vidaj1 = 0;
-                    }
-                    document.getElementById("error").innerHTML = "";
-                    cambiarTamaño(porcentaje, 2);
+                if (energiaj2 < energiaPoder) {
+                    errorElement.innerText = "No tienes suficiente energía";
+                    return;
                 }
+                let daño = dañoPoder + (dañoj2 + vidaj1) * 0.30;
+                let dañoRedondeado = Math.ceil(daño);
+                vidaj1 -= dañoRedondeado;
+
+                if (vidaj1 < 0) {
+                    vidaj1 = 0;
+                }
+
+                energiaj2 -= energiaPoder;
+                energiaj2 += 5;
+                document.getElementById("energia2").innerText = energiaj2;
+                let arrayTurno= ['fumar',2,dañoRedondeado,energiaPoder];
+                array.push(arrayTurno);
+                actualizarBarraVida(1);
             }
+
+            errorElement.innerText = "";
+            turno++;
         }
-    </script>
-</body>
-</html>
+        function morder(jugador) {
+            let dañoPoder = 25;
+            let energiaPoder = 30;
+            let errorElement = document.getElementById("error");
+
+            if (jugador === 1) {
+                if (energiaj1 < energiaPoder) {
+                    errorElement.innerText = "No tienes suficiente energía";
+                    return;
+                }
+                let daño = (dañoPoder +(dañoj1/2)+(vidaj1/6));
+                let dañoRedondeado = Math.ceil(daño);
+                vidaj2 -= dañoRedondeado;
+
+                if (vidaj2 < 0) {
+                    vidaj2 = 0;
+                }
+                energiaj1 -= energiaPoder;
+                energiaj1 += 5;
+                let arrayTurno= ['morder',1,dañoRedondeado,energiaPoder];
+                array.push(arrayTurno);
+                document.getElementById("energia1").innerText = energiaj1;
+                actualizarBarraVida(2);
+            } else {
+                if (energiaj2 < energiaPoder) {
+                    errorElement.innerText = "No tienes suficiente energía";
+                    return;
+                }
+                let daño = (dañoPoder +(dañoj1/2)+(vidaj1/6));
+                let dañoRedondeado = Math.ceil(daño);
+                vidaj1 -= dañoRedondeado;
+
+                if (vidaj1 < 0) {
+                    vidaj1 = 0;
+                }
+
+                energiaj2 -= energiaPoder;
+                energiaj2 += 5;
+                document.getElementById("energia2").innerText = energiaj2;
+                let arrayTurno= ['morder',2,dañoRedondeado,energiaPoder];
+                array.push(arrayTurno);
+                actualizarBarraVida(1);
+            }
+
+            errorElement.innerText = "";
+            turno++;
+        }
+function saltarTurno(jugador) {
+    if (jugador === 1) {
+        energiaj1 += 5; 
+        document.getElementById("energia1").innerText = energiaj1; 
+    } else {
+        energiaj2 += 5; 
+        document.getElementById("energia2").innerText = energiaj2; 
+    }
+
+    turno++; 
+    let arrayTurno= ['saltarTurno',jugador,0,0];
+    array.push(arrayTurno);
+    document.getElementById("numTurno").innerText = turno; 
+}   
+function comprobar() {
+    let botonesDiv = document.getElementById("botones");
+
+    if (vidaj1 <= 0) {
+        botonesDiv.innerHTML = "";
+        let nuevoBoton = document.createElement("button");
+        nuevoBoton.innerText = "¡Jugador 2 Gana!";
+        nuevoBoton.className = "boton";
+        nuevoBoton.onclick = function() {
+            let arrayJSON = encodeURIComponent(JSON.stringify(array));
+            window.location.href = "registrarBatalla.php?ganador=Jugador2&array=" + arrayJSON;
+        };
+        botonesDiv.appendChild(nuevoBoton);
+    }
+
+    if (vidaj2 <= 0) {
+        botonesDiv.innerHTML = "";
+        let nuevoBoton = document.createElement("button");
+        nuevoBoton.innerText = "¡Jugador 1 Gana!";
+        nuevoBoton.className = "boton";
+        nuevoBoton.onclick = function() {
+            let arrayJSON = encodeURIComponent(JSON.stringify(array));
+            window.location.href = "registrarBatalla.php?ganador=Jugador1&array=" + arrayJSON;
+        };
+        botonesDiv.appendChild(nuevoBoton);
+    }
+}
+
+
+
+</script>
---
+++ codigo/inflaestructura/www/registrarBatalla.php
@@ -0,0 +1,5 @@
+<?php
+    $acciones = json_decode(urldecode($_GET['array']), true);
+    var_dump($acciones); 
+    
+?>
