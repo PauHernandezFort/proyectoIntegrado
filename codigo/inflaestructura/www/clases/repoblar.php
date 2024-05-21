ab9c63d1f2b71296e74c54038976b3654498fbe0
sintaxyserror <joagsa@floridauniversitaria.es>
2024-05-21 13:23:56 +0200

    repoblar

--- codigo/inflaestructura/www/clases/repoblar.php
+++ codigo/inflaestructura/www/clases/repoblar.php
@@ -3,28 +3,43 @@
 class Repoblar extends Connection{
 
     public function importarCuentas(){
-        $cuentas = fopen("cuentas.csv","r") or die("Unable to open file!"); 
+        $cuentas = fopen("cuenta.csv", "r") or die("Unable to open file!"); 
         while (($data = fgetcsv($cuentas)) !== false) {
             $query = "INSERT INTO Cuenta (correo, contraseña, nombre) VALUES ('$data[0]', '$data[1]', '$data[2]')";
             $result = mysqli_query($this->conn, $query);
+            if ($result) {
+                echo "Cuenta importada: $data[0], $data[1], $data[2]<br>";
+            } else {
+                echo "Error importando cuenta: " . mysqli_error($this->conn) . "<br>";
+            }
         }
         fclose($cuentas);
     }
 
     public function importarPersonajes() {
-        $personajes = fopen("Personajes.csv","r") or die("Unable to open file!"); 
+        $personajes = fopen("Personajes.csv", "r") or die("Unable to open file!"); 
         while (($data = fgetcsv($personajes)) !== false) {
             $query = "INSERT INTO Personaje (nombre, energia, correocuenta, vida, daño) VALUES ('$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]')";
             $result = mysqli_query($this->conn, $query);
+            if ($result) {
+                echo "Personaje importado: $data[0], $data[1], $data[2], $data[3], $data[4]<br>";
+            } else {
+                echo "Error importando personaje: " . mysqli_error($this->conn) . "<br>";
+            }
         }
         fclose($personajes);
     }
 
     public function importarPoderes(){
-        $poderes = fopen("Poderes.csv","r") or die("Unable to open file!"); 
+        $poderes = fopen("Poderes.csv", "r") or die("Unable to open file!"); 
         while (($data = fgetcsv($poderes)) !== false) {
             $query = "INSERT INTO Poder (nombrePoder, daño, coste, descripcion) VALUES ('$data[0]', '$data[1]', '$data[2]', '$data[3]')";
             $result = mysqli_query($this->conn, $query); 
+            if ($result) {
+                echo "Poder importado: $data[0], $data[1], $data[2], $data[3]<br>";
+            } else {
+                echo "Error importando poder: " . mysqli_error($this->conn) . "<br>";
+            }
         }
         fclose($poderes);
     }
@@ -37,6 +52,12 @@ class Repoblar extends Connection{
         $result1 = mysqli_query($this->conn, $query1);
         $result2 = mysqli_query($this->conn, $query2);
         $result3 = mysqli_query($this->conn, $query3);
+
+        if ($result1 && $result2 && $result3) {
+            echo "Todas las tablas han sido borradas correctamente.<br>";
+        } else {
+            echo "Error borrando las tablas: " . mysqli_error($this->conn) . "<br>";
+        }
     }
 
     public function importar(){
@@ -55,11 +76,13 @@ class Repoblar extends Connection{
         $result2 = mysqli_query($this->conn, $query2)->fetch_row()[0];
         $result3 = mysqli_query($this->conn, $query3)->fetch_row()[0];
         
+        echo "Personajes en la tabla: $result1<br>";
+        echo "Poderes en la tabla: $result2<br>";
+        echo "Cuentas en la tabla: $result3<br>";
+        
         if ($result1 == 0 && $result2 == 0 && $result3 == 0) {
             $this->importar();
         }
     }
 }
 ?>
-
-
