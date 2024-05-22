7ccf96e52090c2bc0f4709204861126babeecbda
Carlos <cagumi@floridauniversitaria.es>
2024-05-22 18:44:20 +0200

    Añadiendo logo a la batalla

--- codigo/inflaestructura/www/batalla.php
+++ codigo/inflaestructura/www/batalla.php
@@ -44,7 +44,15 @@ $energiaj2 = $arrayj2['energia'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
     <title>Batalla</title>
+    <link href="logo.jpeg" rel="icon" type="image/x-icon">
+    <link href="logo.jpeg" rel="apple-touch-icon" sizes="180x180">
+    <link href="logo.jpeg" rel="icon" type="image/png">
+    <meta name="theme-color" content="#343a40">
     <style>
          body {
          background-image: url('escenariobatalla1.jpg');
